6579a85d6d92ce624f91e44f27a6883e0b971738
Mikael Santos <mykaelsantos@outlook.com.br>
2025-06-22 15:47:51 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+
+        int[] resultado = TwoSum(nums, target);
+
+        Console.WriteLine("Índices encontrados: [" + resultado[0] + ", " + resultado[1] + "]");
+    }
+
+    static int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> mapa = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complemento = target - nums[i];
+
+            if (mapa.ContainsKey(complemento))
+            {
+                return new int[] { mapa[complemento], i };
+            }
+
+            mapa[nums[i]] = i;
+        }
+
+        return new int[0]; // Nunca alcançado se sempre existir solução
+    }
+}
